32be63f21bf6c849e3fe1af6fc4b549a9da24b11
rlskoeser <rebecca.s.koeser@emory.edu>
2016-09-20 18:14:05 -0400

    Fix test case that expects system exit to be called

--- tests/baggers/test_lsdi.py
+++ tests/baggers/test_lsdi.py
@@ -1,13 +1,13 @@
 from mock import patch, Mock
+import pytest
 
 from baggins.baggers.lsdi import LsdiBagger
 
 
 class TestLsdiBagger:
 
-    @patch('baggins.baggers.lsdi.exit')
     @patch('baggins.baggers.lsdi.argparse')
-    def test_get_options(self, mockargparse, mockexit):
+    def test_get_options(self, mockargparse):
         mockparser = mockargparse.ArgumentParser.return_value
 
         mockparser.parse_args.return_value = Mock(item_ids=[])
@@ -18,20 +18,17 @@ class TestLsdiBagger:
         # self.options = parser.parse_args()
 
         lbag = LsdiBagger()
-        # no items specified
-        lbag.get_options()
+        # no items specified - should exit
+        with pytest.raises(SystemExit):
+            lbag.get_options()
         mockargparse.ArgumentParser.assert_called_once()
         mockparser.parse_args.assert_called_once()
         mockparser.print_help.assert_called_once()
-        mockexit.assert_called_once()
 
-        mockexit.reset_mock()
         mockoptions = Mock(item_ids=[3031])
         mockparser.parse_args.return_value = mockoptions
         lbag.get_options()
-        mockexit.assert_not_called()
         assert lbag.options == mockoptions
-
 
     # TODO: test process_items method; current functionality is just
     # placeholder logic and will change
